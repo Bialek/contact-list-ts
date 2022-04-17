58f96327c31b50bff5bd8f7bc586ad8a549e4afa
Rafał Białek <37634450+Bialek@users.noreply.github.com>
2022-04-17 14:51:23 +0200

    fix(config): few changes in package and in config (#4)

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
 
 const container = document.getElementById("root");
 
-if (container) {
-  const root = createRoot(container);
-
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  container
+);
